e399c0c88a3ef494653167f0e9bddbe91635b63b
juliocachaydev <julio.cachay.dev@gmail.com>
2025-08-02 18:15:25 -0400

    To Laptop

--- Jcg.Application.Optics/Jcg.Application.Optics/Api/ILens.cs
+++ Jcg.Application.Optics/Jcg.Application.Optics/Api/ILens.cs
@@ -4,51 +4,31 @@ using System.Runtime.InteropServices.ComTypes;
 
 namespace Jcg.Application.Optics.Api
 {
-    public interface ILensState<TData, TProperty>
+    
+    public interface ILens<TRoot, TTarget>
     {
-        TData InitialData { get; }
+        TRoot UpdatedRoot { get; }
         
-        TData MutatedData { get; }
-    }
-    public interface ILens<TData, TProperty> : ILensState<TData, TProperty>
-    {
-        TProperty Value { get; set; }
+        TTarget Value { get; set; }
     }
     
-    public interface ICollectionLens<TData, TProperty> : ILensState<TData, IEnumerable<TProperty>>
-    {
-        IEnumerable<TProperty> Value { get; set; }
-
-        void Remove();
-    }
 
     public static class LensFactory
     {
-        public static ILens<TData, TProperty> CreateLens<TData, TProperty>(
-            this TData initialData,
-            Func<TData, TProperty> selector,
-            Func<TData, TProperty, TData> mutation)
+        public static ILens<TRoot, TTarget> CreateLens<TRoot, TTarget>(
+            this TRoot source,
+            Func<TRoot, TTarget> getter,
+            Func<TRoot, TTarget, TRoot> setter)
         {
             throw new NotImplementedException();
         }
-        
-        
-        public static ILens<TData, TProperty2> FocusProperty<TData, TProperty1, TProperty2>(
-            this ILens<TData, TProperty1> currentLens,
-            Func<TProperty1, TProperty2> selector,
-            Func<TProperty1, TProperty2, TProperty1> mutation)
+
+        public static ILens<TRoot, TTarget> FocusLens<TRoot, TSource, TTarget>(
+            this ILens<TRoot, TSource> lens,
+            Func<TSource, TTarget> getter,
+            Func<TSource, TTarget, TSource> setter)
         {
             throw new NotImplementedException();
         }
-        
-        
-        public static ICollectionLens<TData, TProperty> ToCollectionLens<TData, TProperty>(
-            this ILens<TData, TProperty> currentLens,
-            Func<TData, IEnumerable<TProperty>> selector,
-            Func<TData, IEnumerable<TProperty>, TData> mutation)
-        {
-            throw new NotImplementedException();
-        }
-        
     }
 }
